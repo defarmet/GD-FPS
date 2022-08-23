ab903a6856ca21bbbe5bc554c047d24f2a501029
JaleenCheers <JTCheers@student.fullsail.edu>
2022-08-23 11:40:30 -0400

    Commit

--- Zomderbar FPS/Assets/Scripts/gameManager.cs
+++ Zomderbar FPS/Assets/Scripts/gameManager.cs
@@ -6,80 +6,86 @@ using UnityEngine.UI;
 public class gameManager : MonoBehaviour
 {
 
-	public static gameManager instance;
+    public static gameManager instance;
 
-	public GameObject player;
-	public playerController playerScript;
-	
-	public GameObject pauseMenu;
-	public GameObject currentMenuOpen;
-	public GameObject playerDamageFlash;
-	public GameObject playerDeadMenu;
-	
-	public GameObject currentGunHUD;
-	public GameObject[] gunHUD;
+    public GameObject player;
+    public playerController playerScript;
 
-	public Image playerHpBar;
-	public Image ammoBar;
+    public GameObject pauseMenu;
+    public GameObject currentMenuOpen;
+    public GameObject playerDamageFlash;
+    public GameObject playerDeadMenu;
 
-	public GameObject playerSpawnPoint;
+    public GameObject currentGunHUD;
+    public GameObject[] gunHUD;
 
-	public bool isPaused = false;
+    public Image playerHpBar;
+    public Image ammoBar;
 
-	void Awake()
-	{
-		instance = this;
-		player = GameObject.FindGameObjectWithTag("Player");
-		playerScript = player.GetComponent<playerController>();
+    public GameObject playerSpawnPoint;
 
-		playerSpawnPoint = GameObject.FindGameObjectWithTag("Player Spawn Point");
-		playerScript.respawn();
+    public bool isPaused = false;
 
-	}
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<playerController>();
 
-	void Update()
-	{
-		if (Input.GetButtonDown("Cancel") && playerScript.hp > 0)
-		{
-			isPaused = !isPaused;
-			currentMenuOpen = pauseMenu;
-			currentMenuOpen.SetActive(isPaused);
+        playerSpawnPoint = GameObject.FindGameObjectWithTag("Player Spawn Point");
+        playerScript.respawn();
+
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel") && playerScript.hp > 0)
+        {
+            isPaused = !isPaused;
+            currentMenuOpen = pauseMenu;
+            currentMenuOpen.SetActive(isPaused);
 
             if (isPaused)
                 cursorLockPause();
             else
                 cursorUnlockUnpause();
-            //pause_game(!isPaused);
-		}
-	}
 
-	public void cursorLockPause()
-	{
-		Cursor.visible = true;
-		Cursor.lockState = CursorLockMode.Confined;
-		Time.timeScale = 0;
-	}
+        }
+    }
 
-	public void cursorUnlockUnpause()
-	{
-		Cursor.visible = false;
-		Cursor.lockState = CursorLockMode.Locked;
-		Time.timeScale = 1;
-		currentMenuOpen.SetActive(isPaused);
-		currentMenuOpen = null;
-	}
+    public void cursorLockPause()
+    {
+        if (currentGunHUD != null)
+            currentGunHUD.SetActive(false);
 
-	//public void pause_game(bool p)
-	//{
-	//	Cursor.visible = p;
-	//	isPaused = p;
-	//	if (p) {
-	//		Cursor.lockState = CursorLockMode.Confined;
-	//		Time.timeScale = 0;
-	//	} else {
-	//		Cursor.lockState = CursorLockMode.Locked;
-	//		Time.timeScale = 1;
-	//	}
-	//	currentMenuOpen.SetActive(p);
-	//}
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+        Time.timeScale = 0;
+    }
+
+    public void cursorUnlockUnpause()
+    {
+        if (currentGunHUD != null)
+            currentGunHUD.SetActive(true);
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1;
+        currentMenuOpen.SetActive(isPaused);
+        currentMenuOpen = null;
+    }
+
+    //public void pause_game(bool p)
+    //{
+    //	Cursor.visible = p;
+    //	isPaused = p;
+    //	if (p) {
+    //		Cursor.lockState = CursorLockMode.Confined;
+    //		Time.timeScale = 0;
+    //	} else {
+    //		Cursor.lockState = CursorLockMode.Locked;
+    //		Time.timeScale = 1;
+    //	}
+    //	currentMenuOpen.SetActive(p);
+    //}
 }
