4ff23fd49496c6d45dff46b7a9da299325a532ba
TerryKimGameDev <Tkim@student.fullsail.edu>
2022-09-04 19:33:33 -0400

    door

--- Zomderbar FPS/Assets/Scripts/fallingTrigger.cs
+++ Zomderbar FPS/Assets/Scripts/fallingTrigger.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 public class fallingTrigger : MonoBehaviour
 {
     public int layerToFall; //changes to this layer
+    public GameObject fallingGuy = null; //to see what is in trigger
     private int layerOfObject = 0; //preserve layer of the object entering. defaults to default layer
 
     private void OnTriggerEnter(Collider other)
     {
-        layerOfObject = other.gameObject.layer;
-        other.gameObject.layer = layerToFall;
+        //layerOfObject = other.gameObject.layer;
+        if (other.CompareTag("Player"))
+        {
+            fallingGuy = other.gameObject;
+            other.gameObject.layer = layerToFall;
+            StartCoroutine(resetLayer());
+        }
     }
-    private void OnTriggerExit(Collider other)
+
+    
+    //private void OnTriggerExit(Collider other) //seems to no longer work???
+    //{
+    //    if (other.CompareTag("Player"))
+    //        fallingGuy = null;
+    //        other.gameObject.layer = 0;
+    //}
+
+    IEnumerator resetLayer() //temp fix until I figure it out
     {
-        other.gameObject.layer = layerOfObject;
+        yield return new WaitForSeconds(.8f);
+        fallingGuy.layer = 0;
     }
 }
