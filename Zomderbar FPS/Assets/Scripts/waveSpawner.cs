e56b2ee14d2f2d64aabd8206350c8840ce4c8dfb
JaleenCheers <JTCheers@student.fullsail.edu>
2022-09-09 13:44:25 -0400

    Update waveSpawner.cs

--- Zomderbar FPS/Assets/Scripts/waveSpawner.cs
+++ Zomderbar FPS/Assets/Scripts/waveSpawner.cs
@@ -7,48 +7,75 @@ public class waveSpawner : MonoBehaviour
     [SerializeField] int totalWaves;
     [SerializeField] GameObject[] enemies;
     [SerializeField] int spawnInterval;
-    [SerializeField] int enemiesToSpawn;
+    [SerializeField] int enemiesInWave;
+    [SerializeField] List<GameObject> enemiesToSpawn;
 
-    public Transform[] spawnLocation;
+    public GameObject[] spawnLocation;
 
     public int enemiesSpawned;
-    public int currWave = 1;
-    bool activateSpanwer;
+    public int currWave;
+    public bool activateSpanwer = false;
+    private bool firstSpawn = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && !firstSpawn)
         {
             activateSpanwer = true;
+            firstSpawn = true;
         }
     }
 
-    private void Start()
-    {
 
-    }
 
     private void Update()
     {
-        if (activateSpanwer)
+        if (firstSpawn && !activateSpanwer && gameManager.instance.enemyKilled == enemiesInWave)
         {
-            while (currWave < totalWaves && activateSpanwer)
-            {
-                SpawnWave();
-            }
+            activateSpanwer = true;
+            gameManager.instance.enemyKilled = 0;
+            ++currWave;
+
+        }
+
+        if (activateSpanwer && currWave <= totalWaves)
+        {
+            
+            activateSpanwer = false;
+            StartCoroutine(SpawnWave());
+
+            
+            
+            
+
         }
     }
 
+    void GenerateEnemies()
+    {
+        enemiesToSpawn.Clear();
+        for (int i = 0; i < enemiesInWave; ++i)
+        {
+            
+            int enemy = Random.Range(0, enemies.Length);
+            enemiesToSpawn.Add(enemies[enemy]);
+        }
+    }
+
+
     IEnumerator SpawnWave()
     {
-       
-        while (enemiesSpawned < enemiesToSpawn)
+        GenerateEnemies();
+        for (int i = 0; i < enemiesToSpawn.Count; ++i)
         {
-            int enemy = Random.Range(0, enemies.Length);
-            int spawn = Random.Range(0, spawnLocation.Length);
-            Instantiate(enemies[enemy], spawnLocation[spawn].transform.position, Quaternion.identity);
+
+            int spawnLoc = Random.Range(0, spawnLocation.Length);
+            Instantiate(enemiesToSpawn[i], spawnLocation[spawnLoc].transform.position, Quaternion.identity);
             enemiesSpawned++;
+            gameManager.instance.enemyCount++;
             yield return new WaitForSeconds(spawnInterval);
+
+
         }
 
     }
