39c470e0e4955ec27ef9d6172af1453d4abe1197
TerryKimGameDev <Tkim@student.fullsail.edu>
2022-09-11 10:40:47 -0400

    main changes

---
+++ Zomderbar FPS/Assets/Scripts/maskObj.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class maskObj : MonoBehaviour
+{
+    public GameObject[] MaskedObj;
+    void Start()
+    {
+        for (int  i = 0; i < MaskedObj.Length; i++)
+        {
+            MaskedObj[i].GetComponent<MeshRenderer>().material.renderQueue = 3002;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
